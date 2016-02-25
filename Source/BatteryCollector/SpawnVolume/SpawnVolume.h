b08eb17858f69c52b2ef72842c44861e84051526
trancesnail <hiryu@qq.com>
2016-02-25 11:41:57 +0800

    SpawnTest

---
+++ Source/BatteryCollector/SpawnVolume/SpawnVolume.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+UCLASS()
+class BATTERYCOLLECTOR_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnVolume();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	FORCEINLINE class UBoxComponent * GetWhereToSpawn() const { return WhereToSpawn; }
+
+	UFUNCTION(BlueprintPure, Category = "Spawning")
+	FVector GetRandomPotinInVolume();
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Spawning")
+	TSubclassOf<class APickup> WhatToSpawn;
+
+	FTimerHandle SpawnTimer;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	float SpawnDelayRangeLow;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	float SpawnDelayRangeHigh;
+
+private:
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly, Category = "Spawning",meta = ( AllowPrivateAccess = "true" ))
+	class UBoxComponent *  WhereToSpawn;
+
+	void SpawnPickup();
+
+	float SpawnDelay;
+
+};
